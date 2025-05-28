6257b424e4f32b3e188a4f11f8b1931d3b6261f1
informatica25 <informaticapc.2025@gmail.com>
2025-05-28 10:17:57 -0500

    Backend del modulo de quejas

--- Backend/app/routes/__init__.py
+++ Backend/app/routes/__init__.py
@@ -1,8 +1,10 @@
 from .usuarios import usuarios_bp
 from .anuncio_routes import anuncio_bp
 from .reconocimiento_routes import reconocimiento_bp
+from app.routes.queja_routes import queja_bp
 
 def register_routes(app):
     app.register_blueprint(usuarios_bp)
     app.register_blueprint(anuncio_bp)
     app.register_blueprint(reconocimiento_bp)
+    app.register_blueprint(queja_bp)

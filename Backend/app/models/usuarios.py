4a18645bc98b4821c7f2d85d0978107af7299000
MaxAntony23 <maxmonterola@gmail.com>
2025-06-13 21:54:48 -0500

    borrar codigo de alumno en usuario

--- Backend/app/models/usuarios.py
+++ Backend/app/models/usuarios.py
@@ -10,5 +10,4 @@ class Usuario(db.Model):
     contraseña = db.Column(db.String(255), nullable=False)
     rol = db.Column(db.String(20), nullable=False)
     fecha_cumpleaños = db.Column(db.Date)
-    codigo_alumno = db.Column(db.String(20), unique=True)
 
